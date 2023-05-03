fa4cc4b0d7626bb063b6ede292eae3c5452d7a58
Ritvik Prabhu <ritvik@ritvikprabhu.com>
2023-05-02 22:10:11 -0400

    Deployment branch

--- webpage/frontend/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "@/styles/globals.css";
-
-// All code in this file was written by Amrita Ballurkar
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
